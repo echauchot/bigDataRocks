e391eb8d96c695bf1e4441036722b7b0d1aea009
Etienne Chauchot <etienne.chauchot@free.fr>
2016-05-20 15:02:36 +0200

    added cleaning of index after test

--- importer/src/test/java/service/ElasticSearchWriteIT.java
+++ importer/src/test/java/service/ElasticSearchWriteIT.java
@@ -1,6 +1,10 @@
 package service;
 
+import bigdatarocks.common.bean.Person;
+import bigdatarocks.common.dao.PersonElasticsearchDao;
 import bigdatarocks.importer.pipeline.WritePipeline;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,13 +12,30 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class ElasticSearchWriteIT {
-    @Test
-    public void percistToElasticSearch() throws IOException {
-        WritePipeline pipeliine = new WritePipeline();
-        pipeliine.run("src/main/resources/input/persons.json", false, true);
 
+    private static WritePipeline pipeline;
+    private static PersonElasticsearchDao personElasticsearchDao;
 
-
+    @BeforeClass
+    public static void initTest(){
+        pipeline = new WritePipeline();
+        personElasticsearchDao = new PersonElasticsearchDao("172.17.0.3", "9300", "elasticsearch");
+        personElasticsearchDao.init(Person.class);
     }
 
+    @Test
+    public void percistToElasticSearch() throws IOException {
+        pipeline.run("src/main/resources/input/persons.json", false, true);
+        long count = personElasticsearchDao.count();
+        assertEquals("wrong number of persons in elasticsearch", 12L, count);
+        Person person = personElasticsearchDao.read("Etienne");
+        assertEquals("wrong userName", "Etienne", person.getUserName());
+        assertEquals("wrong age", 32, person.getAge());
+        assertEquals("wrong CildrenCont", 1, person.getChildrenCount());
+    }
+
+    @AfterClass
+    public static void clean(){
+        personElasticsearchDao.deleteAll();
+    }
 }
