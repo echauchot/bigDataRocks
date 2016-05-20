3d3be1b46ac57b19e03d1eece446fea53dd0ed4d
Etienne Chauchot <etienne.chauchot@free.fr>
2016-05-20 15:49:31 +0200

    added cleaning of table after test

--- importer/src/test/java/service/CassandraWriteServiceIT.java
+++ importer/src/test/java/service/CassandraWriteServiceIT.java
@@ -3,6 +3,9 @@ package service;
 import bigdatarocks.common.bean.Person;
 import bigdatarocks.common.dao.PersonCassandraDao;
 import bigdatarocks.importer.pipeline.WritePipeline;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -12,18 +15,29 @@ import static org.junit.Assert.assertEquals;
 
 public class CassandraWriteServiceIT {
 
+    private static WritePipeline pipeline;
+    private static PersonCassandraDao personCassandraDao;
+
+    @BeforeClass
+    public static void initTest() {
+        pipeline = new WritePipeline();
+        personCassandraDao = new PersonCassandraDao("localhost", "9042", CASS_KEYSPACE);
+        personCassandraDao.init(Person.class);
+    }
+
     @Test
     public void percistToCassandra() throws IOException {
-        WritePipeline pipeliine = new WritePipeline();
-        pipeliine.run("src/main/resources/input/persons.json", true, false);
-        PersonCassandraDao personCassandraDao = new PersonCassandraDao("localhost", "9042", CASS_KEYSPACE);
-        personCassandraDao.init(Person.class);
+        pipeline.run("src/main/resources/input/persons.json", true, false);
         long count = personCassandraDao.count();
-        assertEquals("wrong number of persons inserted", 12, count);
+        assertEquals("wrong number of persons inserted", 12L, count);
         Person etienne = personCassandraDao.read("Etienne");
         assertEquals("wrong age for Etienne", 32, etienne.getAge());
         assertEquals("wrong childrenCount for Etienne", 1, etienne.getChildrenCount());
 
+    }
 
+    @AfterClass
+    public static void clean(){
+        personCassandraDao.deleteAll();
     }
 }
